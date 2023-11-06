4b3a724e9f53427e139f3d8123f36ffa0a7d86b7
KyunghyunKimRRC <kkim60@academic.rrc.ca>
2023-11-06 10:45:53 -0600

    Update routes.rb
    Changed routes for all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :customers, only: %i[index show]
+  get 'customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized_customers'
+  resources :customers, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
